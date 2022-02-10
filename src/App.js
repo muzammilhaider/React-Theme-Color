0c4a918b1b982dba79a799f7c053b466561cfcfa
Muzammil Haider <muzammilhaider110@gmail.com>
2022-02-10 10:14:32 +0500

    React change theme color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isActive,setActive] = useState("false");
+  let dataChange = () => {
+    setActive(!isActive);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={isActive ? "myClass" : 'myClass2'}>
+      <button onClick={dataChange}>THEME CHANGE</button>
     </div>
   );
 }
